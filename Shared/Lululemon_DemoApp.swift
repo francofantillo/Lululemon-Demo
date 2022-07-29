5c2fc3c96d003f582905db732e2468444995a4e0
Franco <franco.fantillo@gmail.com>
2022-07-29 14:46:38 -0700

    Initial Commit

---
+++ Shared/Lululemon_DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  Lululemon_DemoApp.swift
+//  Shared
+//
+//  Created by Franco Fantillo on 2022-07-29.
+//
+
+import SwiftUI
+
+@main
+struct Lululemon_DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
